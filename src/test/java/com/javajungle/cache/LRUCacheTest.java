c959f8c5968c2a035782d3976cd81bad509afe76
Huz Maryna <maryna.huz.dev@gmail.com>
2023-06-13 21:10:46 +0300

    delete redundant annotation

--- src/test/java/com/javajungle/cache/LRUCacheTest.java
+++ src/test/java/com/javajungle/cache/LRUCacheTest.java
@@ -1,7 +1,6 @@
 package com.javajungle.cache;
 
 import com.javajungle.counter.CharCounter;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -25,9 +24,6 @@ class LRUCacheTest {
         MockitoAnnotations.openMocks(this);
         cache = new LRUCache<>(charCounter, 3);
     }
-
-    @AfterEach
-
 
     @Test
     void get_shouldReturnStoredValue() {
