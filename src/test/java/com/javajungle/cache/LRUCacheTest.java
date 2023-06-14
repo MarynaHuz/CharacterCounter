8efeaca475599b1e2536291d383829e436ad6817
Huz Maryna <maryna.huz.dev@gmail.com>
2023-06-14 19:06:15 +0300

    update and simplify test cases

--- src/test/java/com/javajungle/cache/LRUCacheTest.java
+++ src/test/java/com/javajungle/cache/LRUCacheTest.java
@@ -1,100 +1,52 @@
 package com.javajungle.cache;
 
-import com.javajungle.counter.CharCounter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 
 class LRUCacheTest {
 
-    @Mock
-    CharCounter charCounter;
+    private final Cache<Integer, Integer> cache = new LRUCache<>(3);
 
-    private LRUCache<String, Map<Character, Integer>> cache;
+    @Test
+    void should_AddValues() {
+        cache.put(1, 11);
+        cache.put(2, 22);
+        cache.put(3, 33);
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        cache = new LRUCache<>(charCounter, 3);
+        assertEquals(11, cache.get(1));
+        assertEquals(22, cache.get(2));
+        assertEquals(33, cache.get(3));
     }
 
     @Test
-    void get_shouldReturnStoredValue() {
-        String key = "Key";
-        Map<Character, Integer> value = Map.of('A', 1, 'B', 2, 'C', 3);
-        cache.put(key, value);
+    void should_EvictLeastAdded() {
+        cache.put(1, 11);
+        cache.put(2, 22);
+        cache.put(3, 33);
+        cache.put(4, 44);
 
-        Map<Character, Integer> expectedResult = value;
-
-        Map<Character, Integer> result = cache.get(key);
-
-        assertEquals(expectedResult, result);
-        verify(charCounter, never()).count(anyString());
-    }
-
-
-    @Test
-    void get_shouldReturnNullIfKeyNotPresent() {
-        String key = "Key";
-
-        Map<Character, Integer> result = cache.get(key);
-
-        assertNull(result);
-        verify(charCounter, never()).count(anyString());
+        assertFalse(cache.containsKey(1));
+        assertEquals(22, cache.get(2));
+        assertEquals(33, cache.get(3));
+        assertEquals(44, cache.get(4));
     }
 
     @Test
-    void put_shouldStoreValueInCache() {
-        String key = "Key";
-        Map<Character, Integer> value = Map.of('A', 1, 'B', 2, 'C', 3);
+    void should_EvictLeastUsed() {
+        cache.put(1, 11);
+        cache.put(2, 22);
+        cache.put(3, 33);
 
-        cache.put(key, value);
+        cache.get(1);
+        cache.get(2);
 
-        assertTrue(cache.containsKey(key));
-        verify(charCounter, never()).count(anyString());
-    }
+        cache.put(4, 44);
 
-    @Test
-    void put_shouldReplaceValueIfKeyAlreadyExists() {
-        String key = "Key";
-        Map<Character, Integer> firstValue = Map.of('A', 1, 'B', 2, 'C', 3);
-        Map<Character, Integer> secondValue = Map.of('X', 10, 'Y', 20, 'Z', 30);
-
-        cache.put(key, firstValue);
-        cache.put(key, secondValue);
-
-        assertTrue(cache.containsKey(key));
-        assertEquals(secondValue, cache.get(key));
-        verify(charCounter, never()).count(anyString());
-    }
-
-    @Test
-    void containsKey_shouldReturnTrueForExistingKey() {
-        String key = "Key";
-        Map<Character, Integer> value = Map.of('A', 1, 'B', 2, 'C', 3);
-
-        cache.put(key, value);
-
-        assertTrue(cache.containsKey(key));
-        verify(charCounter, never()).count(anyString());
-    }
-
-    @Test
-    void containsKey_shouldReturnFalseForNonexistentKey() {
-        String key = "Key";
-        String nonexistentKey = "NonexistentKey";
-        Map<Character, Integer> value = Map.of('A', 1, 'B', 2, 'C', 3);
-
-        cache.put(key, value);
-
-        assertFalse(cache.containsKey(nonexistentKey));
-        verify(charCounter, never()).count(anyString());
+        assertFalse(cache.containsKey(3));
+        assertEquals(11, cache.get(1));
+        assertEquals(22, cache.get(2));
+        assertEquals(44, cache.get(4));
     }
 }
